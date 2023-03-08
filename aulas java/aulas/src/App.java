e3beb5c5af6a4399b0559acc243def2d99e9a25c
João Marcos <112454993+joaomarcospsza@users.noreply.github.com>
2023-03-08 14:52:54 -0300

    Teste

--- aulas java/aulas/src/App.java
+++ aulas java/aulas/src/App.java
@@ -2,5 +2,9 @@ public class App {
     public static void main(String[] args){
         int contagem = 0; //Iniciando um objeto
         Carro camaro = new Carro(); // Instanciando um objeto
+        camaro.cor = "Preto";
+        Anunciante jao = new Anunciante();
+        jao.nome = "João";
+        System.out.println(jao.nome);
     }
 }
