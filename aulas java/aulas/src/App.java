d10aa42ed2a02693f4cdf0922a7c238f8dc9ddcf
João Marcos <112454993+joaomarcospsza@users.noreply.github.com>
2023-03-07 22:26:38 -0300

    Aula sobre classes, objetos e Instanciação

---
+++ aulas java/aulas/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args){
+        int contagem = 0; //Iniciando um objeto
+        Carro camaro = new Carro(); // Instanciando um objeto
+    }
+}
---
+++ aulas java/aulas/src/Carro.java
@@ -0,0 +1,12 @@
+public class Carro {
+    
+    String cor;
+    int qtde_porta;
+    String marca;
+    String modelo;
+    int ano;
+    int kilometragem;
+    String observacao;
+    double preco;
+
+}
